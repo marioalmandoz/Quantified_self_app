805eaf7cb367492da6533aa06d3d81e0b7728742
Nigel Proveniers <497262@student.fontys.nl>
2023-11-07 22:11:10 +0100

    fixed the scaling and bugs of the breathing exercise

--- DuoCase9/DuoCase9/BreathingApp.swift
+++ DuoCase9/DuoCase9/BreathingApp.swift
@@ -4,25 +4,17 @@ struct ContentView: View {
     @State private var isBreathing = false
     @State private var countdown = 4
     @State private var phase = "Breathe In"
-    @State private var circlePosition: CGSize = CGSize(width: -90, height: -90) // Initial position (top-left corner)
+    @State private var circlePosition: CGSize = CGSize(width: -140, height: -140) // Initial position (top-left corner slightly above the border)
 
     var body: some View {
         VStack {
-            Text(isBreathing ? phase : "Press to start")
-                .font(.largeTitle)
-                .foregroundColor(.blue)
-                .onTapGesture {
-                    self.isBreathing.toggle()
-                    self.countdown = 4
-                    self.phase = "Breathe In"
-                    self.startCountdown()
-                }
-                .padding()
+            Spacer()
 
             ZStack {
                 Rectangle()
-                    .frame(width: 200, height: 200)
+                    .frame(width: 300, height: 300) // Updated dimensions of the square
                     .foregroundColor(.green)
+                    .cornerRadius(20)
                     .overlay(
                         VStack {
                             Text(phase)
@@ -35,9 +27,16 @@ struct ContentView: View {
                                 .foregroundColor(.white)
                         }
                     )
+                    .onTapGesture {
+                        self.isBreathing.toggle()
+                        self.countdown = 4
+                        self.phase = "Breathe In"
+                        self.circlePosition = CGSize(width: -140, height: -140) // Set initial position for the circle (slightly above the top-left corner above the border)
+                        self.startCountdown()
+                    }
 
                 Circle()
-                    .frame(width: 20, height: 20)
+                    .frame(width: 40, height: 40) // Adjusted size of the circle
                     .foregroundColor(.red)
                     .offset(circlePosition)
                     .animation(.linear(duration: 1))
@@ -55,13 +54,13 @@ struct ContentView: View {
                 // Calculate circle position based on the current phase
                 withAnimation {
                     if self.phase == "Breathe In" {
-                        self.circlePosition = CGSize(width: 90, height: -90) // Top-right corner (Hold)
+                        self.circlePosition = CGSize(width: 140, height: -140) // Top-right corner above the border
                     } else if self.phase == "Hold" {
-                        self.circlePosition = CGSize(width: 90, height: 90) // Bottom-right corner (Breathe Out)
+                        self.circlePosition = CGSize(width: 140, height: 140) // Bottom-right corner above the border
                     } else if self.phase == "Breathe Out" {
-                        self.circlePosition = CGSize(width: -90, height: 90) // Bottom-left corner (Hold 2)
+                        self.circlePosition = CGSize(width: -140, height: 140) // Bottom-left corner above the border
                     } else {
-                        self.circlePosition = CGSize(width: -90, height: -90) // Top-left corner (Breathe In)
+                        self.circlePosition = CGSize(width: -140, height: -140) // Top-left corner above the border
                     }
                 }
                 
