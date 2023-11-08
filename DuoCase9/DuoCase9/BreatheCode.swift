2ea44a0e51d5bd86e9009a9cc1ce053f4c869987
Nigel Proveniers <497262@student.fontys.nl>
2023-11-08 13:50:58 +0100

    finised the breathing exercise page with design and functionality

--- DuoCase9/DuoCase9/BreatheCode.swift
+++ DuoCase9/DuoCase9/BreatheCode.swift
@@ -3,25 +3,67 @@ import SwiftUI
 struct MainPageView: View {
     var body: some View {
         NavigationView {
+            
             VStack {
-                Spacer()
+                
+                Text("Breathing Exercises")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                
+                Text("It’s time to relax.                Here are some different ways to breathe: ")
+                    .padding(.top, 2)
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .frame(minWidth: 0, maxWidth: 230, alignment: .center)
+                
+                Spacer().frame(height: 40)
                 
                 NavigationLink(destination: ContentView()) {
-                    Text("Start Breathing")
+                    Text("To sleep:  4-7-8 (military)")
                         .font(.title)
                         .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
+                        .padding(30)
+                        .background(Color(hex: 0x435585))
                         .cornerRadius(10)
+                        .frame(maxWidth: 260)
+                }
+                
+                Spacer().frame(height: 20)
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("Reduce stress:   3-5-6")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(30)
+                        .background(Color(hex: 0x435585))
+                        .cornerRadius(10)
+                        .frame(minWidth: 250,maxWidth: 260)
+                }
+                
+                Spacer().frame(height: 20)
+                
+                NavigationLink(destination: ContentView()) {
+                    Text("Reduce anxiety: 4-4-4-4")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(30)
+                        .background(Color(hex: 0x435585))
+                        .cornerRadius(10)
+                        .frame(minWidth: 250,maxWidth: 260)
                 }
                 
                 Spacer()
             }
-            .navigationBarTitle("Breathing App")
+            .padding()
+            .background(Color(hex: 0x132043))
         }
+
     }
 }
-
 struct MainPageView_Previews: PreviewProvider {
     static var previews: some View {
         MainPageView()
